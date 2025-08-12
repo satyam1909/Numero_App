76d8a39467414cc980566aa15ce840bf1c64258f
satyam1909 <satyam1909@gmail.com>
2025-08-12 18:57:03 +0530

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Numero_App/', // Add this line for GitHub Pages
+  base: '/Numero_App/',
 })
